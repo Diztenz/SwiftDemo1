a726dfd1fc96638e3bcb6f085040adf7553cec4b
Chuck <cosmoc05@yahoo.com>
2021-03-15 12:10:00 -0700

    Initial Commit

---
+++ SwiftDemo1/SwiftDemo1App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDemo1App.swift
+//  SwiftDemo1
+//
+//  Created by Charlie Caquias on 3/15/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDemo1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
