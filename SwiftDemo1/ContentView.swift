398034aa2c71065ba885cf479d0d72ee1bdaa741
Chuck <cosmoc05@yahoo.com>
2021-03-21 09:19:41 -0700

    Initial commit

--- SwiftDemo1/ContentView.swift
+++ SwiftDemo1/ContentView.swift
@@ -8,14 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var showIcon = false
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(spacing: 50) {
+            if showIcon {
+            Image("logo")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 170, height: 170)
+                .clipped()
+                .padding(.top, 180)
+            }
+            Spacer()
+            VStack(alignment: .leading, spacing: 10) {
+                Text("I love SwiftUI!")
+                    .font(.largeTitle)
+                    .foregroundColor(.blue)
+                Text("SwiftUI makes developing iOS apps fun and easy!")
+                
+            }
+            .frame(width: 300, height: 150)
+                Button(action: {showIcon.toggle()}, label: {
+                Text("Show me the logo")
+                    .padding()
+                    .background(Color.blue)
+                    .cornerRadius(10)
+                    .foregroundColor(.white)
+                
+            })
+            
+            }
+        .padding(.bottom, 130)
+        }
     }
-}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
